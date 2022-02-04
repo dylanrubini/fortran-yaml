5fc32b292f5813102c160ca98353e43276c16483
dylanrubini <dylan.rubini@hotmai.co.uk>
2022-02-04 10:47:18 +0000

    change name

--- yaml_version.F90
+++ yaml_version.F90
@@ -1,4 +1,4 @@
 module yaml_version
-   character(len=*),parameter :: git_commit_id = "0.1.0"
+   character(len=*),parameter :: git_commit_id = ""
    character(len=*),parameter :: git_branch_name = "unknown"
 end module
